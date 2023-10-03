405c0f7c17b5344f0fbeba36be354854e19b2c96
ThomasSA-Git <thom45w9@stud.kea.dk>
2023-10-03 20:17:08 +0200

    test mand

--- Pokemon/util/preparePages.js
+++ Pokemon/util/preparePages.js
@@ -1,10 +1,20 @@
 import { readPage, renderPage } from "./templateEngine.js";
 
 const frontPage = readPage('./public/pages/frontpage/frontpage.html');
-export const frontpagePage = renderPage(frontPage);
+export const frontpagePage = renderPage(frontPage, 
+    {
+        tabTitle: "PoKEAmon | Welcome"
+    });
 
 const battle = readPage('./public/pages/battle/battle.html');
-export const battlePage = renderPage(battle);
+export const battlePage = renderPage(battle, 
+    {
+        tabTitle: "PoKEAmon | Battle",
+        cssLinks: `<link rel="stylesheet" href="/pages/battle/battle.css">`
+    });
 
 const contact = readPage('./public/pages/contact/contact.html');
-export const contactPage = renderPage(contact);
+export const contactPage = renderPage(contact, 
+    {
+        tabTitle: "PoKEAmon | Contact"
+    });
